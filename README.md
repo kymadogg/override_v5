a7e30fdab640667aed7ab0b2f29c176024eaaef8
kmhswimgirl <kmherbstzuber@wpi.edu>
2026-05-26 00:17:04 -0400

    added basic launch file + field wall file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # override_v5
-Gazebo Sim Harmonic version of the 2026-27 VEX Robotics Competition game "Override"
+Gazebo Sim Harmonic + ROS 2 Jazzy version of the 2026-27 VEX Robotics Competition game "Override"
+
+## Launching the world:
+```bash
+ros2 launch override_v5 world_select.launch.py world:=override
+```
+
---
+++ launch/world_select.launch.py
@@ -0,0 +1,52 @@
+import os
+
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+    # base file path for the package
+    vex_path = os.path.join(get_package_share_directory('override_v5'))
+
+    # secondary file paths for locating resources
+    models_path = os.path.join(vex_path, 'models')
+    worlds_path = os.path.join(vex_path, 'worlds')
+
+    def get_available_worlds():
+        try:
+            world_list = [f[:-4] for f in os.listdir(worlds_path) if f.endswith('.sdf')]
+            if not world_list:
+                return "directory empty"
+            return "Gz Sim Worlds: " + ", ".join(world_list)
+        except Exception as e:
+            return f"ERROR: {e}"
+        
+    worlds = get_available_worlds()
+
+    # set gz sim resource path
+    gz_sim_resource = SetEnvironmentVariable(
+        name = 'GZ_SIM_RESOURCE_PATH',
+        value=f"{models_path}:{worlds_path}:{vex_path}"
+    )
+
+    # arguments for gz sim
+    arguments = LaunchDescription([
+        DeclareLaunchArgument('world', default_value='empty', description=worlds),
+    ])
+
+    # actually run gazebo
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
+        launch_arguments = [
+            ('gz_args', [LaunchConfiguration('world'),'.sdf',' -v 4',' -r'])
+        ]
+    )
+    # launch each item defined above by returning the variable
+    return LaunchDescription([
+        gz_sim_resource,
+        arguments,
+        gazebo
+    ])
